ab10d7c22eaee43d2e1145036d437e79c5f170fd
Omar Khawaja <khawaja910@gmail.com>
2022-05-24 09:22:42 -0400

    add copyright

--- examples/prereqs_quickstart/k8s-cluster/variables.tf
+++ examples/prereqs_quickstart/k8s-cluster/variables.tf
@@ -1,3 +1,10 @@
+/**
+ * Copyright © 2014-2022 HashiCorp, Inc.
+ *
+ * This Source Code is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this project, you can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 variable "add_master_webhook_firewall_rules" {
   type        = bool
   default     = true
